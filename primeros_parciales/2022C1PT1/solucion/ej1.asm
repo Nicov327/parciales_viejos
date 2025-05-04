95e3e79c31bdef5fbdf08a05a205a42c4ab8341a
Nicolás <nicolasjoannas14@gmail.com>
2025-05-04 05:55:27 -0300

    Ahora sí, strArrayAddLast corre pasando todos los tests

--- primeros_parciales/2022C1PT1/solucion/ej1.asm
+++ primeros_parciales/2022C1PT1/solucion/ej1.asm
@@ -4,7 +4,7 @@ extern strClone
 extern strLen
 global strArrayNew                 ;   DESCOMENTAR PARA IMPLEMENTAR LA SOLUCIÓN EN ASM
 global strArrayGetSize
-;global strArrayAddLast
+global strArrayAddLast
 ;global strArraySwap
 ;global strArrayDelete
 
@@ -13,6 +13,9 @@ strArray_SIZE EQU 0
 strArray_CAPACITY EQU 1
 strArray_DATA EQU 8
 
+ARRAY_OFFSET_SIZE EQU 0
+ARRAY_OFFSET_CAPACITY EQU 1
+ARRAY_OFFSET_DATA EQU 8
 
 
 ;########### SECCION DE DATOS
@@ -76,68 +79,46 @@ strArrayGetSize:
 ; void  strArrayAddLast(str_array_t* a, char* data)     a -> rdi  data -> rsi
 strArrayAddLast:
     push rbp
+    mov rbp, rsp
     push r12
     push r13
     push r14
-    push r15
-    mov rbp, rsp
-    sub rsp, 8      ;   Alineo la pila (hice una cantidad impar de pushes, por eso tengo que alinear nuevamente a 16 bytes)
+    sub rsp, 8
 
-    mov r12, rdi    ;   r12 contiene 'a'
-    mov r13, rsi    ;   r13 contiene 'data'
+    mov r12, rdi                        ;   Muevo a r12 el puntero del struct ('a')
+    mov r13, rsi                        ;   Muevo a r13 el puntero del string a copiar ('data')
 
     xor r8, r8
-    mov r8w, WORD [r12]  ;   En la parte baja del r8, tengo tanto el size (0-7) como el capacity (8-15)
     xor r9, r9
-    mov r9b, r8b    ;   Cargo el size en el byte de abajo de r9
-    shr r8, 8       ;   Muevo 8 lugares a la derecha el r8 (Para que capacity quede en el byte de abajo)
+    mov r8b, BYTE [r12 + strArray_SIZE]
+    mov r9b, BYTE [r12 + strArray_CAPACITY]
+    cmp r9b, r8b
+    jle .fin                            ;   Si capacity <= size entonces no tengo nada por hacer y me voy a .fin
 
-    cmp r8b, r9b    ;   Seteo flags acorde a la operación r8 - r9
-    jle .fin
 
-.hayEspacio:
-    mov rdi, rsi    ;   Necesito saber cuánto mide el string a ser copiado, por lo que muevo su puntero para ser dado al strLen
-    call strLen
-    mov rdi, rax    ;   Obtenida la longitud, pido esa cantidad de bytes por malloc
-    mov r15, rax    ;   r15 contiene ahora la longitud del string a copiar
-    inc rdi         ;   Necesito un byte más para el null terminator del string a copiar (ej: Si mi string mide 6, necesito 7 bytes)
-    call malloc
-    mov r14, rax    ;   Me guardo en r14 el puntero para el nuevo string
-    
-    mov r8, [r12 + strArray_DATA]   ;   Cargo en r8 el puntero de 'data' del strArray
+    mov rdi, r13                        ;   Muevo el puntero de 'data' al rsi para darselo al strClone
+    call strClone
+    mov r14, rax
 
-    mov rax, 8      ;   Tamaño (en bytes) de un puntero
     xor r9, r9
-    mov r9b, byte [r12]              ;   Cargo el size
-    mul r9       ;   Multiplico tal tamaño por la cantidad de lugares ocupados en la tabla 'data'
+    mov r9b, BYTE [r12 + strArray_SIZE] ;   Cargo el size en byte bajo de r9
+    xor rax, rax
+    mov al, 8
+    mul r9b                             ;   rax = 8. Multiplico por el size para obtener el offset que necesito hacer en a->data
 
-    add r8, rax     ;   Del puntero de 'data', me desplazo la cantidad de lugares correspondientes
+    mov r8, [r12 + strArray_DATA]
+    add r8, rax
+    mov [r8], r14
 
-    mov [r8], r14   ;   Escribo en el array data del strArray la dirección del puntero al nuevo string
-
-    add BYTE [r12 + strArray_SIZE], 1   ;   Incremento en 1 el size (pues agrego un nuevo puntero)
-
-.clonoString:
-    cmp r15, 0      ;   Si ya no quedan caracteres por copiar, me voy al fin
-    je .fin
-
-    mov r8b, BYTE [r13]  ;   Cargo en r8b el caracter del string a ser clonado
-    mov BYTE [r14], r8b  ;   Lo guardo en el puntero que fue escrito en la tabla 'data'
-
-    sub r15, 1      ;   Descuento un caracter a ser procesado
-    inc r13         ;   Me muevo un byte tanto en ambos punteros
-    inc r14
-
-    jmp .clonoString
+    inc BYTE [r12 + strArray_SIZE]
 
 .fin:
     add rsp, 8
-    pop r15
     pop r14
     pop r13
     pop r12
     pop rbp
-
+    ret
 
 ; void  strArraySwap(str_array_t* a, uint8_t i, uint8_t j)
 ;strArraySwap:
